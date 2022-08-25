6f82c30302321a6954683b59c65ff5d00478f7cb
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-25 18:24:59 +0530

    prime number program

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const arr=[];
+ const primeNumber=()=>
+ {
+    for(var i=1;i<100;i++)
+    {
+      var flag=0;
+      for(var j=2;j<=i/2;j++)
+      {
+       if(i%j===0)
+       {
+        flag=1;
+        break;
+       }
+      }
+      if(flag===0)
+      {
+        arr.push(i); 
+      }
+    }
+ }
+primeNumber();
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <div>{arr.map(item=>
+   <p>{item}</p>  
+   )}</div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
